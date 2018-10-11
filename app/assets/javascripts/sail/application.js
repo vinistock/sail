56d2708785e0d253219621cb61b1bd1f776651b7
Vinicius Stock <instantink+pair+vinicius@hp.com>
2018-10-11 16:51:03 -0300

    Remove jquery and use pure javascript

--- app/assets/javascripts/sail/application.js
+++ app/assets/javascripts/sail/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require rails-ujs
 //= require_tree .
