dca69bf3b262cddb0c9fbff9cf64a2752469f42f
Vinicius Stock <vinicius.stock@outlook.com>
2018-11-26 20:04:12 -0200

    Try to fix travis build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,7 @@ RSpec.configure do |config|
   end
 
   Capybara.register_driver :headless_chrome do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: %w(headless disable-gpu) })
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: %w(headless disable-gpu no-sandbox disable-dev-shm-usage) })
     Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
   end
 
