1cb68f7f849a97b83b7316c9a1241eabd5914d2c
Vinicius Stock <instantink+pair+vinicius@hp.com>
2018-12-21 13:30:35 -0200

    Add auto submit for search

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -11,5 +11,9 @@ module Dummy
       config.load_defaults 5.1
       config.active_record.sqlite3.represent_boolean_as_integer = true
     end
+
+    Sail.configure do |config|
+      config.enable_search_auto_submit = true
+    end
   end
 end
