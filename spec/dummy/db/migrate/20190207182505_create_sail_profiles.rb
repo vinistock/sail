12d89c166703e5081b765a296b6114400b3837fd
Vinicius Stock <instantink+pair+vinicius@hp.com>
2019-02-07 17:10:19 -0200

    Create profile and entry models

--- spec/dummy/db/migrate/20190207182505_create_sail_profiles.rb
+++ spec/dummy/db/migrate/20190207182505_create_sail_profiles.rb
@@ -2,8 +2,8 @@ class CreateSailProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :sail_entries do |t|
       t.string :value, null: false
-      t.references :setting, foreign_key: true
-      t.references :profile, foreign_key: true
+      t.references :setting, index: true
+      t.references :profile, index: true
       t.timestamps
     end
 
@@ -12,5 +12,8 @@ class CreateSailProfiles < ActiveRecord::Migration[5.2]
       t.index ["name"], name: "index_sail_profiles_on_name", unique: true
       t.timestamps
     end
+
+    add_foreign_key(:sail_entries, :sail_settings, column: :setting_id)
+    add_foreign_key(:sail_entries, :sail_profiles, column: :profile_id)
   end
 end
---
+++ spec/models/sail/profile_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+describe Sail::Profile, type: :model do
+
+end
