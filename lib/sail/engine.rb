7771b4ac5bb6e31a96985bbdfc8080354bdd2f13
Vinicius Stock <vinicius.stock@outlook.com>
2019-02-12 20:35:37 -0200

    Add profiles button to dashboard
    * Add cog icon
    * Add profiles button
    * Refactor search buttons style into shared class

--- lib/sail/engine.rb
+++ lib/sail/engine.rb
@@ -14,7 +14,7 @@ module Sail
 
     initializer "sail.assets.precompile" do |app|
       app.config.assets.precompile += %w[sail/refresh.svg sail/sort.svg sail/angle-left.svg
-                                         sail/angle-right.svg sail/link.svg]
+                                         sail/angle-right.svg sail/link.svg sail/cog.svg]
     end
 
     initializer "sail" do
