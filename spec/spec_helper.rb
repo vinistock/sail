dbffb661abbe0025fef604ec7e66d9afa15c2417
Vinicius Stock <vinistock@users.noreply.github.com>
2019-06-07 13:38:16 -0300

    Fix responsive layout for cards and nav buttons (#116)
    * Hide nav buttons for small screens
    * Properly set the width of cards for responsive break points

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,7 +58,7 @@ RSpec.configure do |config|
 
   Capybara.register_driver :headless_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage] }
+      chromeOptions: { args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage window-size=1920,1180] }
     )
 
     Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
