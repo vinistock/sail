48080b06439042731a7dbf7ae329da6074a61b0d
Vinicius Stock <vinicius.stock@outlook.com>
2019-07-03 11:38:54 -0300

    Start setting the locale inside dummy app [ci skip]

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   def index
     random_setting = Sail::Setting.pluck(:name)
     5.times { Sail.get(random_setting.sample) }
+  end
+
+  protected
+
+  def set_locale
+    I18n.locale = params[:locale] || :en
   end
 end
