6f15214cec369d8271cebcff46c0b41e0dbc02e7
Vinicius Stock <vinistock@users.noreply.github.com>
2019-09-26 15:34:46 -0400

    Add logo (#138)

--- lib/sail/engine.rb
+++ lib/sail/engine.rb
@@ -25,7 +25,7 @@ module Sail
 
     initializer "sail.assets.precompile" do |app|
       app.config.assets.precompile += %w[sail/refresh.svg sail/sort.svg sail/angle-left.svg
-                                         sail/angle-right.svg sail/link.svg sail/cog.svg]
+                                         sail/angle-right.svg sail/link.svg sail/cog.svg sail/favicon.png]
     end
 
     initializer "sail" do
