afa4bc1ad4b59944e7fbbe393ad14b1b730c86d6
Vinicius Stock <vinicius.stock@outlook.com>
2019-11-25 19:48:27 -0500

    Add switcher to graphql

--- lib/sail/graphql.rb
+++ lib/sail/graphql.rb
@@ -7,16 +7,33 @@ module Sail
   # Module to include type definitions
   # for GraphQL APIs.
   module Graphql
-    extend ActiveSupport::Concern
+    module Types
+      extend ActiveSupport::Concern
 
-    included do
-      field :sail_get, ::GraphQL::Types::JSON, null: true do
-        description "Returns the value for a given setting."
-        argument :name, String, required: true, description: "The setting's name."
-      end
+      included do
+        field :sail_get, ::GraphQL::Types::JSON, null: true do
+          description "Returns the value for a given setting."
+          argument :name, String, required: true, description: "The setting's name."
+        end
 
-      def sail_setting(name:)
-        Sail.get(name)
+        field :sail_switcher, ::GraphQL::Types::JSON, null: true do
+          description "Switches between the positive or negative setting based on the throttle."
+          argument :positive, String, required: true, description: "The setting's name if the throttle is bigger than the desired amount."
+          argument :negative, String, required: true, description: "The setting's name if the throttle is smaller than the desired amount."
+          argument :throttled_by, String, required: true, description: "The throttle setting's name."
+        end
+
+        def sail_get(name:)
+          Sail.get(name)
+        end
+
+        def sail_switcher(positive:, negative:, throttled_by:)
+          Sail.switcher(
+            positive: positive,
+            negative: negative,
+            throttled_by: throttled_by
+          )
+        end
       end
     end
   end
