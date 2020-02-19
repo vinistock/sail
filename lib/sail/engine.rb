759550e196ce4d2560a068d3407a49e1545a0a39
Vinicius Stock <vinicius.stock@outlook.com>
2020-02-18 20:37:10 -0500

    Fix application.js extension in precompile list

--- lib/sail/engine.rb
+++ lib/sail/engine.rb
@@ -25,7 +25,7 @@ module Sail
     initializer "sail.assets.precompile" do |app|
       app.config.assets.precompile += %w[sail/refresh.svg sail/sort.svg sail/angle-left.svg
                                          sail/angle-right.svg sail/link.svg sail/cog.svg sail/application.css
-                                         sail/application.js]
+                                         sail/application.js.erb]
     end
 
     initializer "sail" do
