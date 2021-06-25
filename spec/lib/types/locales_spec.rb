e3133284d92cc9737f7e8ed53c43311571545984
Vinicius Stock <stock@hey.com>
2021-06-25 19:23:34 -0400

    Fix locale offense

--- spec/lib/types/locales_spec.rb
+++ spec/lib/types/locales_spec.rb
@@ -4,10 +4,10 @@ describe Sail::Types::Locales, type: :lib do
   let(:setting) { Sail::Setting.create!(name: :setting, cast_type: :array, value: "en;es") }
 
   describe "#to_value" do
-    subject { described_class.new(setting).to_value }
-
-    before do
-      I18n.locale = locale
+    subject do
+      I18n.with_locale(locale) do
+        described_class.new(setting).to_value
+      end
     end
 
     context "when locale is included" do
